ae712b6a8ab498972209bef9542eda11126341f8
florianmarkusse <florianmarkusse@hotmail.com>
2023-12-02 21:11:49 +0100

    use bear

---
+++ example/main.c
@@ -0,0 +1,16 @@
+#include "log.h"
+
+// flo_Command flo_addCommands(flo_Command command, flo_String command);
+
+int main() {
+    //   flo_Command command;
+    //   command = flo_addCommands(command, "cc");
+    //   command = flo_addCommands(command, "-o mydick");
+    //   command = flo_addCommands(command, "example.c");
+
+    flo_String someString = FLO_STRING("things");
+
+    FLO_INFO("nerdddddddddddd\n", FLO_FLUSH);
+
+    return 0;
+}
---
+++ notes.md
@@ -0,0 +1,1 @@
+cc -o first example/main.c -Ilibs/util/include -Llibs/util/build -lutil-Release
